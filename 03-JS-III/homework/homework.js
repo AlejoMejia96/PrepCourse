f41659ab4e05c2f951387bcd66188fde16136de0
Alejandro Quintero Mejía <93171512+AlejoMejia96@users.noreply.github.com>
2022-09-05 22:30:48 -0500

    Update homework.js

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -12,7 +12,7 @@ function imprimirSumaNumeros() {
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
-  // en el arreglado de enteros pasado como parámetro
+  // en el arreglo de enteros pasado como parámetro
   // Tu código:
 }
 
